8a13bd465b54c1c8e41c199435882a908aa240d0
CG <96789431+CeeGitau@users.noreply.github.com>
2024-06-01 22:17:47 +0300

    Moved Login link to the right most side of navbar

--- src/source/navbar.js
+++ src/source/navbar.js
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ backgroundColor: "#f0f0f0", padding: "10px" }}>
-      <Link
-        style={{ marginRight: "10px", color: "#333", textDecoration: "none" }}
-        to="/"
-      >
-        Home
-      </Link>
-      <Link style={{ color: "#333", textDecoration: "none" }} to="/favourites">
-        Favourite Cities
-      </Link>
-      <Link
-        style={{ color: "#333", textDecoration: "none" }}
-        to="/login"
-      >
-        Login
-      </Link>
+    <nav style={navStyle}>
+      <div style={leftLinksStyle}>
+        <Link style={linkStyle} to="/">Home</Link>
+        <Link style={linkStyle} to="/favourites">Favourite Cities</Link>
+      </div>
+      <Link style={linkStyle} to="/login">Login</Link>
     </nav>
   );
 }
+
+const navStyle = {
+  backgroundColor: "#f0f0f0",
+  padding: "10px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const leftLinksStyle = {
+  display: "flex",
+};
+
+const linkStyle = {
+  marginRight: "10px",
+  color: "#333",
+  textDecoration: "none",
+};
